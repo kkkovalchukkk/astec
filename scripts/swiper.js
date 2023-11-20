ce09698233319c036569fb05d131c0b0a9d8f602
kkkovalchukkk <student.kovalchuk@yandex.ru>
2023-11-20 19:18:05 +0700

    added contacts page

--- scripts/swiper.js
+++ scripts/swiper.js
@@ -1,9 +1,15 @@
-const projectSwiper = new Swiper('.project-section__swiper', {
-  slidesPerView: 1,
-  loop: true,
-  spaceBetween: 20,
-  navigation: {
-    nextEl: '.project-section__swiper-navigation-btn-right',
-    prevEl: '.project-section__swiper-navigation-btn-left',
-  },
-});
+document.querySelector('.project-section__swiper') &&
+  new Swiper('.project-section__swiper', {
+    slidesPerView: 1,
+    loop: true,
+    spaceBetween: 20,
+    navigation: {
+      nextEl: '.project-section__swiper-navigation-btn-right',
+      prevEl: '.project-section__swiper-navigation-btn-left',
+    },
+  });
+document.querySelector('.detail-section__gallery') &&
+  new Swiper('.detail-section__gallery', {
+    slidesPerView: 'auto',
+    spaceBetween: 20,
+  });
